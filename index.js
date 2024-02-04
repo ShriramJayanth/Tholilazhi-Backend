bb0a0b70131bfbbde29a0cdf9a2171eae3cd061c
ShriramJayanth <csshriramjayanth.cse2022@citchennai.net>
2024-02-04 16:37:04 +0530

    database configured

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 3001;
 
+dotenv.config();
 app.use(express.json());
 app.use(cors());
 
@@ -19,8 +22,9 @@ app.use(cors());
 
 
 
-app.listen(PORT, () => {
-    console.log('====================================');
-    console.log("Server is running on port: " + PORT);
-    console.log('====================================');
-})
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
